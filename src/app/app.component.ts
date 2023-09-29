dd56a32242d2dfc0fb43b37a3ec209a924d544b2
christy1329 <dentaparengkuan5@gmail.com>
2023-09-29 10:00:58 +0700

    Injeksi theme ke Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularTheme';
+  email = 'folabessy26@gmail.com';
+  nim = '212102428';
+  nama = 'Endricho';
 }
